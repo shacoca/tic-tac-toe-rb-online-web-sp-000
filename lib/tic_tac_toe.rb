9b233cf3ff9f571ae1a962c5a5fabb0e2ff2c863
shawn campbell <shacoca@yahoo.com>
2020-08-06 02:35:37 +0000

    Done.

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -19,3 +19,7 @@ def display_board(board = [" "," "," "," "," "," "," "," "," "])
   puts line
   puts " #{board[6]} | #{board[7]} | #{board[8]} "
 end
+
+def input_to_index(input)
+  index = input.to_i - 1
+end
