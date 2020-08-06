f0bd25e002e48a82292c2b9809575fed63a49cba
shawn campbell <shacoca@yahoo.com>
2020-08-06 02:31:31 +0000

    Done.

---
+++ lib/tic_tac_toe.rb
@@ -0,0 +1,21 @@
+WIN_COMBINATIONS = [
+  [0,1,2], #top row
+  [3,4,5], #middle row
+  [6,7,8], #bottom row
+
+  [0,3,6], #left column
+  [1,4,7], #middle column
+  [2,5,8], #right column
+
+  [0,4,8], #NW - SE
+  [2,4,6] #NE - SW
+]
+
+def display_board(board = [" "," "," "," "," "," "," "," "," "])
+  line = "-----------"
+  puts " #{board[0]} | #{board[1]} | #{board[2]} "
+  puts line
+  puts " #{board[3]} | #{board[4]} | #{board[5]} "
+  puts line
+  puts " #{board[6]} | #{board[7]} | #{board[8]} "
+end
